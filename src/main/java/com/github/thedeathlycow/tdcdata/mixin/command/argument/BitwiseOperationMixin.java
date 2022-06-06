b2a45bce42840e7411e4e1fc66e56f00aa02e1be
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-06-05 23:29:52 -1000

    bitwise scoreboard ops

---
+++ src/main/java/com/github/thedeathlycow/tdcdata/mixin/command/argument/BitwiseOperationMixin.java
@@ -0,0 +1,53 @@
+package com.github.thedeathlycow.tdcdata.mixin.command.argument;
+
+import net.minecraft.command.argument.OperationArgumentType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+@Mixin(OperationArgumentType.class)
+public class BitwiseOperationMixin {
+
+    @ModifyArg(
+            method = "listSuggestions",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/command/CommandSource;suggestMatching([Ljava/lang/String;Lcom/mojang/brigadier/suggestion/SuggestionsBuilder;)Ljava/util/concurrent/CompletableFuture;"
+            )
+    )
+    private String[] addBitwiseOperatorsToSuggestions(String[] candidates) {
+        String[] bitwiseSuggestions = {"<<", ">>", ">>>", "&", "|", "^"};
+        return Stream.concat(Arrays.stream(candidates), Arrays.stream(bitwiseSuggestions))
+                .toArray(String[]::new);
+    }
+
+    @Inject(
+            method = "getIntOperator",
+            at = @At(
+                value = "HEAD"
+            ),
+            cancellable = true
+    )
+    private static void parseBitwiseOperators(String operator, CallbackInfoReturnable<OperationArgumentType.IntOperator> cir) {
+
+        OperationArgumentType.IntOperator parsedOperator = switch (operator) {
+            case "<<" -> (a, b) -> a << b;
+            case ">>" -> (a, b) -> (a >> b);
+            case ">>>" -> (a, b) -> (a >>> b);
+            case "&" -> (a, b) -> a & b;
+            case "|" -> (a, b) -> a | b;
+            case "^" -> (a, b) -> a ^ b;
+            default -> null;
+        };
+
+        if (parsedOperator != null) {
+            cir.setReturnValue(parsedOperator);
+        }
+    }
+
+}
