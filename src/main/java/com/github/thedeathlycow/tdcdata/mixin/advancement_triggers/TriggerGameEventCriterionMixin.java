856f830bfd6a0e533aa1fc9f3027421a79c0f1c9
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-07-11 18:37:48 -1000

    change trigger vibration mixin name

--- src/main/java/com/github/thedeathlycow/tdcdata/mixin/advancement_triggers/TriggerGameEventCriterionMixin.java
+++ src/main/java/com/github/thedeathlycow/tdcdata/mixin/advancement_triggers/TriggerGameEventCriterionMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerWorld.class)
-public class TriggerVibrationCriterionMixin {
+public class TriggerGameEventCriterionMixin {
 
     @Inject(
             method = "emitGameEvent",
