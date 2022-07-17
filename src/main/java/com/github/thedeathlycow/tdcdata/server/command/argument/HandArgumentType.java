e9853602824cb19389c71b4453d9e28f9c193dad
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-07-16 21:09:01 -1000

    HandArgumentType#parse throws a command syntax exception when cannot parse a hand

--- src/main/java/com/github/thedeathlycow/tdcdata/server/command/argument/HandArgumentType.java
+++ src/main/java/com/github/thedeathlycow/tdcdata/server/command/argument/HandArgumentType.java
@@ -4,9 +4,11 @@ import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.minecraft.command.CommandSource;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 
 import java.util.Arrays;
@@ -15,6 +17,11 @@ import java.util.concurrent.CompletableFuture;
 
 public class HandArgumentType implements ArgumentType<Hand> {
     private static final Collection<String> EXAMPLES = Arrays.asList("mainhand", "offhand");
+    private static final DynamicCommandExceptionType NOT_A_HAND_EXCEPTINON = new DynamicCommandExceptionType(
+            (handName) -> {
+                return Text.literal(String.format("%s is not a valid hand", handName));
+            }
+    );
 
     private HandArgumentType() {
     }
@@ -28,14 +35,14 @@ public class HandArgumentType implements ArgumentType<Hand> {
     }
 
     @Override
-    public Hand parse(StringReader stringReader) {
+    public Hand parse(StringReader stringReader) throws CommandSyntaxException {
         String hand = stringReader.readUnquotedString();
         if (hand.equals("mainhand")) {
             return Hand.MAIN_HAND;
         } else if (hand.equals("offhand")) {
             return Hand.OFF_HAND;
         } else {
-            throw new IllegalStateException(String.format("%s is not a valid hand", hand));
+            throw NOT_A_HAND_EXCEPTINON.create(hand);
         }
     }
 
