408e2abeb1c72a8b50c4b48d9ac702ae9f39739d
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-07-16 19:09:45 -1000

    use item criterion

--- src/main/java/com/github/thedeathlycow/tdcdata/advancement/TdcDataAdvancementTriggers.java
+++ src/main/java/com/github/thedeathlycow/tdcdata/advancement/TdcDataAdvancementTriggers.java
@@ -1,10 +1,14 @@
 package com.github.thedeathlycow.tdcdata.advancement;
 
+import com.github.thedeathlycow.tdcdata.DatapackExtensions;
 import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.advancement.criterion.ItemCriterion;
+import net.minecraft.util.Identifier;
 
 public class TdcDataAdvancementTriggers {
 
     public static final PlayerTriggerGameEventCriterion TRIGGER_VIBRATION = new PlayerTriggerGameEventCriterion();
+    public static final UseItemCriterion USE_ITEM = new UseItemCriterion();
 
     public static void registerTriggers() {
         Criteria.register(TRIGGER_VIBRATION);
--- src/main/java/com/github/thedeathlycow/tdcdata/scoreboard/stat/UseStat.java
+++ src/main/java/com/github/thedeathlycow/tdcdata/scoreboard/stat/UseStat.java
@@ -1,10 +1,12 @@
 package com.github.thedeathlycow.tdcdata.scoreboard.stat;
 
 import com.github.thedeathlycow.tdcdata.DatapackExtensions;
+import com.github.thedeathlycow.tdcdata.advancement.TdcDataAdvancementTriggers;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.TypedActionResult;
 
@@ -15,6 +17,7 @@ public class UseStat {
         UseItemCallback.EVENT.register((player, world, hand) -> {
             if (!world.isClient) {
                 player.incrementStat(TdcDataCustomStats.USE);
+                TdcDataAdvancementTriggers.USE_ITEM.trigger((ServerPlayerEntity) player, player.getStackInHand(hand));
             }
             return TypedActionResult.pass(ItemStack.EMPTY);
         });
