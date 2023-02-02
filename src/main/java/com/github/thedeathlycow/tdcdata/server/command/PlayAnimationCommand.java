9a077362632005575cd5b05263097b2490681f57
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-02-02 00:12:37 -1000

    playanimation cleanup

--- src/main/java/com/github/thedeathlycow/tdcdata/server/command/PlayAnimationCommand.java
+++ src/main/java/com/github/thedeathlycow/tdcdata/server/command/PlayAnimationCommand.java
@@ -1,13 +1,12 @@
 package com.github.thedeathlycow.tdcdata.server.command;
 
+import com.github.thedeathlycow.tdcdata.mixin.invokers.LivingEntityInvoker;
 import com.github.thedeathlycow.tdcdata.server.command.argument.HandArgumentType;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
-import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
-import net.minecraft.client.util.math.Vector3d;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityStatuses;
@@ -15,28 +14,15 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.PolarBearEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
-import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
-import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
-import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.Vec3d;
-
-import java.util.Objects;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class PlayAnimationCommand {
-
-    private static final String SWING_SUCCESS = "Animated swing of %s's %s";
-    private static final String HURT_SUCCESS = "Animated hurt for %s";
-    private static final String WARN_SUCCESS = "Animated warn for %s with state %s";
 
     private static final DynamicCommandExceptionType FAILED_ENTITY_EXCEPTION = new DynamicCommandExceptionType(
             (targetName) -> {
@@ -47,25 +33,25 @@ public class PlayAnimationCommand {
 
     private static final DynamicCommandExceptionType NOT_POLAR_BEAR_ENITY_EXCEPTION = new DynamicCommandExceptionType(
             (targetName) -> {
-                return Text.literal(String.format("%s is not a polar bear", targetName));
+                return Text.empty()
+                        .append((Text) targetName)
+                        .append(Text.literal(" is not a polar bear"));
             }
     );
 
     private static final DynamicCommandExceptionType TARGET_IS_DEAD_EXCEPTION = new DynamicCommandExceptionType(
             (targetName) -> {
-                return Text.literal(String.format("%s is not alive", targetName));
-            }
-    );
-
-    private static final DynamicCommandExceptionType TARGET_IS_NOT_DAMAGEABLE_EXCEPTION = new DynamicCommandExceptionType(
-            (targetName) -> {
-                return Text.literal(String.format("%s is not damageable", targetName));
+                return Text.empty()
+                        .append((Text) targetName)
+                        .append(Text.literal(" is not alive"));
             }
     );
 
     private static final DynamicCommandExceptionType SAME_TARGET_EXCEPTION = new DynamicCommandExceptionType(
             (targetName) -> {
-                return Text.literal(String.format("%s cannot ride itself", targetName));
+                return Text.empty()
+                        .append((Text) targetName)
+                        .append(Text.literal(" cannot ride itself"));
             }
     );
 
@@ -75,13 +61,8 @@ public class PlayAnimationCommand {
 
     private static final DynamicCommandExceptionType CANNOT_RIDE_EXCEPTION = new DynamicCommandExceptionType(
             (target) -> {
-                Text targetName = Text.of(target.toString());
-                if (target instanceof Text targetText) {
-                    targetName = targetText;
-                }
-
                 return Text.empty()
-                        .append(targetName)
+                        .append((Text) target)
                         .append(Text.literal(" is not rideable"));
             }
     );
@@ -197,7 +178,7 @@ public class PlayAnimationCommand {
 
         if (target instanceof LivingEntity livingTarget) {
             if (!target.isAlive()) {
-                throw TARGET_IS_DEAD_EXCEPTION.create(target.getDisplayName().getString());
+                throw TARGET_IS_DEAD_EXCEPTION.create(target.getDisplayName());
             }
 
             livingTarget.swingHand(hand, true);
@@ -213,30 +194,29 @@ public class PlayAnimationCommand {
     }
 
     private static int executeHurt(final ServerCommandSource source, Entity target) throws CommandSyntaxException {
-
-        if (target instanceof LivingEntity livingTarget) {
-            if (!target.isAlive()) {
-                throw TARGET_IS_DEAD_EXCEPTION.create(target.getDisplayName().getString());
-            }
-
-            livingTarget.getWorld().sendEntityStatus(livingTarget, EntityStatuses.DAMAGE_FROM_GENERIC_SOURCE);
-
-            Text msg = Text.literal(String.format(HURT_SUCCESS, target.getDisplayName().getString()));
-            source.sendFeedback(msg, true);
-
-            return 1;
-        } else {
-            throw FAILED_ENTITY_EXCEPTION.create(target.getDisplayName().getString());
+        if (!target.isAlive()) {
+            throw TARGET_IS_DEAD_EXCEPTION.create(target.getDisplayName());
         }
+
+        target.getWorld().sendEntityStatus(target, EntityStatuses.DAMAGE_FROM_GENERIC_SOURCE);
+
+        if (target instanceof LivingEntityInvoker invoker) {
+            invoker.tdcdata$invokePlayHurtSound(DamageSource.GENERIC);
+        }
+
+        Text msg = Text.literal("Played hurt animation for ")
+                .append(target.getDisplayName());
+        source.sendFeedback(msg, true);
+
+        return 1;
     }
 
     private static int executeJump(final ServerCommandSource source, Entity target) throws CommandSyntaxException {
 
         if (target instanceof MobEntity mob) {
             mob.getJumpControl().setActive();
-            Text msg = Text.empty()
-                    .append(target.getDisplayName())
-                    .append(Text.literal(" started jumping"));
+            Text msg = Text.literal("Played jump animation for ")
+                    .append(target.getDisplayName());
             source.sendFeedback(msg, true);
         } else {
             throw CANNOT_JUMP_EXCEPTION.create();
@@ -248,27 +228,29 @@ public class PlayAnimationCommand {
 
         if (target instanceof PolarBearEntity polarBear) {
             if (!target.isAlive()) {
-                throw TARGET_IS_DEAD_EXCEPTION.create(target.getDisplayName().getString());
+                throw TARGET_IS_DEAD_EXCEPTION.create(target.getDisplayName());
             }
 
             polarBear.setWarning(state);
 
-            Text msg = Text.literal(String.format(WARN_SUCCESS, target.getDisplayName().getString(), state));
+            Text msg = Text.literal("Set warning animation state for ")
+                    .append(target.getDisplayName())
+                    .append(Text.literal(String.format(" to %s", state)));
             source.sendFeedback(msg, true);
 
             return 1;
         } else {
-            throw NOT_POLAR_BEAR_ENITY_EXCEPTION.create(target.getDisplayName().getString());
+            throw NOT_POLAR_BEAR_ENITY_EXCEPTION.create(target.getDisplayName());
         }
     }
 
     private static int executeMount(final ServerCommandSource source, Entity target, Entity vehicle, boolean force) throws CommandSyntaxException {
 
         if (target.getId() == vehicle.getId()) {
-            throw SAME_TARGET_EXCEPTION.create(target.getDisplayName().getString());
+            throw SAME_TARGET_EXCEPTION.create(target.getDisplayName());
         }
         if (target.startRiding(vehicle, force)) {
-            Text msg = Text.literal("") // new way of making text lol
+            Text msg = Text.empty()
                     .append(target.getDisplayName())
                     .append(Text.literal(" started riding "))
                     .append(vehicle.getDisplayName());
