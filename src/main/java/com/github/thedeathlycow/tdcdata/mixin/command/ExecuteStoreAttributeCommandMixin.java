cc8e5c8a613f9b2e6ab05a6a59616f8ef2e55afa
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-02-02 03:34:13 -1000

    execute store attribute now accepts modifiers

--- src/main/java/com/github/thedeathlycow/tdcdata/mixin/command/ExecuteStoreAttributeCommandMixin.java
+++ src/main/java/com/github/thedeathlycow/tdcdata/mixin/command/ExecuteStoreAttributeCommandMixin.java
@@ -2,11 +2,14 @@ package com.github.thedeathlycow.tdcdata.mixin.command;
 
 import com.github.thedeathlycow.tdcdata.server.command.ExecuteStoreAttributeCommand;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.command.argument.RegistryKeyArgumentType;
+import net.minecraft.command.argument.UuidArgumentType;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.server.command.ExecuteCommand;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.registry.Registry;
@@ -14,30 +17,98 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
+
 @Mixin(ExecuteCommand.class)
 public abstract class ExecuteStoreAttributeCommandMixin {
 
     @Inject(
-            method="addStoreArguments",
-            at=@At("HEAD")
+            method = "addStoreArguments",
+            at = @At("HEAD")
     )
-    private static void addAttributeStoreArgument(LiteralCommandNode<ServerCommandSource> node, LiteralArgumentBuilder<ServerCommandSource> builder, boolean requestResult, CallbackInfoReturnable<ArgumentBuilder<ServerCommandSource, ?>> cir){
+    private static void addAttributeStoreArgument(LiteralCommandNode<ServerCommandSource> node, LiteralArgumentBuilder<ServerCommandSource> builder, boolean requestResult, CallbackInfoReturnable<ArgumentBuilder<ServerCommandSource, ?>> cir) {
         builder.then(literal("tdcdata.attribute").then(
                 argument("target", EntityArgumentType.entity()).then(
-                        argument("attribute", RegistryKeyArgumentType.registryKey(Registry.ATTRIBUTE_KEY)).then(
-                                literal("base").then(
-                                        argument("scale", DoubleArgumentType.doubleArg())
-                                                .redirect(node, context -> ExecuteStoreAttributeCommand.executeStoreAttribute(
-                                                        context.getSource(),
-                                                        EntityArgumentType.getEntity(context, "target"),
-                                                        RegistryKeyArgumentType.getAttribute(context, "attribute"),
-                                                        DoubleArgumentType.getDouble(context, "scale"),
-                                                        requestResult
-                                                ))
+                        argument("attribute", RegistryKeyArgumentType.registryKey(Registry.ATTRIBUTE_KEY))
+                                .then(
+                                        literal("base").then(
+                                                argument("scale", DoubleArgumentType.doubleArg())
+                                                        .redirect(node, context -> ExecuteStoreAttributeCommand.executeStoreAttribute(
+                                                                context.getSource(),
+                                                                EntityArgumentType.getEntity(context, "target"),
+                                                                RegistryKeyArgumentType.getAttribute(context, "attribute"),
+                                                                DoubleArgumentType.getDouble(context, "scale"),
+                                                                requestResult,
+                                                                null
+                                                        ))
+                                        )
                                 )
-                        ))
+                                .then(
+                                        literal("modifier").then(
+                                                argument("uuid", UuidArgumentType.uuid())
+                                                        .then(
+                                                                argument("name", StringArgumentType.string())
+                                                                        .then(
+                                                                                argument("scale", DoubleArgumentType.doubleArg())
+                                                                                        .then(
+                                                                                                literal("add").redirect(
+                                                                                                        node, context -> ExecuteStoreAttributeCommand.executeStoreAttribute(
+                                                                                                                context.getSource(),
+                                                                                                                EntityArgumentType.getEntity(context, "target"),
+                                                                                                                RegistryKeyArgumentType.getAttribute(context, "attribute"),
+                                                                                                                DoubleArgumentType.getDouble(context, "scale"),
+                                                                                                                requestResult,
+                                                                                                                (value) -> new EntityAttributeModifier(
+                                                                                                                        UuidArgumentType.getUuid(context, "uuid"),
+                                                                                                                        StringArgumentType.getString(context, "name"),
+                                                                                                                        value,
+                                                                                                                        EntityAttributeModifier.Operation.ADDITION
+                                                                                                                )
+                                                                                                        )
+                                                                                                )
+                                                                                        )
+                                                                                        .then(
+                                                                                                literal("multiply").redirect(
+                                                                                                        node, context -> ExecuteStoreAttributeCommand.executeStoreAttribute(
+                                                                                                                context.getSource(),
+                                                                                                                EntityArgumentType.getEntity(context, "target"),
+                                                                                                                RegistryKeyArgumentType.getAttribute(context, "attribute"),
+                                                                                                                DoubleArgumentType.getDouble(context, "scale"),
+                                                                                                                requestResult,
+                                                                                                                (value) -> new EntityAttributeModifier(
+                                                                                                                        UuidArgumentType.getUuid(context, "uuid"),
+                                                                                                                        StringArgumentType.getString(context, "name"),
+                                                                                                                        value,
+                                                                                                                        EntityAttributeModifier.Operation.MULTIPLY_TOTAL
+                                                                                                                )
+                                                                                                        )
+                                                                                                )
+                                                                                        )
+                                                                                        .then(
+                                                                                                literal("multiply_base").redirect(
+                                                                                                        node, context -> ExecuteStoreAttributeCommand.executeStoreAttribute(
+                                                                                                                context.getSource(),
+                                                                                                                EntityArgumentType.getEntity(context, "target"),
+                                                                                                                RegistryKeyArgumentType.getAttribute(context, "attribute"),
+                                                                                                                DoubleArgumentType.getDouble(context, "scale"),
+                                                                                                                requestResult,
+                                                                                                                (value) -> new EntityAttributeModifier(
+                                                                                                                        UuidArgumentType.getUuid(context, "uuid"),
+                                                                                                                        StringArgumentType.getString(context, "name"),
+                                                                                                                        value,
+                                                                                                                        EntityAttributeModifier.Operation.MULTIPLY_BASE
+                                                                                                                )
+                                                                                                        )
+                                                                                                )
+                                                                                        )
+                                                                        )
+
+                                                        )
+                                        )
+                                )
+                )
         ));
     }
 }
--- src/main/java/com/github/thedeathlycow/tdcdata/server/command/ExecuteStoreAttributeCommand.java
+++ src/main/java/com/github/thedeathlycow/tdcdata/server/command/ExecuteStoreAttributeCommand.java
@@ -1,32 +1,72 @@
 package com.github.thedeathlycow.tdcdata.server.command;
 
 import com.mojang.brigadier.ResultConsumer;
+import com.mojang.brigadier.exceptions.Dynamic3CommandExceptionType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 public class ExecuteStoreAttributeCommand {
     private static final BinaryOperator<ResultConsumer<ServerCommandSource>> BINARY_RESULT_CONSUMER = (resultConsumer, resultConsumer2) -> (context, success, result) -> {
         resultConsumer.onCommandComplete(context, success, result);
         resultConsumer2.onCommandComplete(context, success, result);
     };
-    public static ServerCommandSource executeStoreAttribute(ServerCommandSource source, Entity target, EntityAttribute attribute, double scale, boolean requestResult){
+
+    private static final Dynamic3CommandExceptionType MODIFIER_ALREADY_PRESENT_EXCEPTION = new Dynamic3CommandExceptionType((entityName, attributeName, uuid) -> {
+        return Text.translatable("commands.attribute.failed.modifier_already_present", uuid, attributeName, entityName);
+    });
+
+    public static ServerCommandSource executeStoreAttribute(
+            ServerCommandSource source,
+            Entity target,
+            EntityAttribute attribute,
+            double scale,
+            boolean requestResult,
+            @Nullable Function<Double, EntityAttributeModifier> modifierFactory
+    ) {
         return source.mergeConsumers((context, success, result) -> {
-            double value = (requestResult ?  result : (success ? 1 : 0)) * scale;
-            if (target instanceof LivingEntity livingEntity){
+            double value = (requestResult ? result : (success ? 1 : 0)) * scale;
+            if (target instanceof LivingEntity livingEntity) {
                 EntityAttributeInstance inst = livingEntity.getAttributeInstance(attribute);
-                if (inst != null) {
-                    inst.setBaseValue(value);
-                } else {
-                    source.sendError(Text.translatable("commands.attribute.failed.no_attribute", livingEntity.getName(), Text.translatable(attribute.getTranslationKey())));
+                if (inst == null) {
+                    source.sendError(
+                            Text.translatable(
+                                    "commands.attribute.failed.no_attribute",
+                                    livingEntity.getName(),
+                                    Text.translatable(attribute.getTranslationKey())
+                            )
+                    );
+                    return;
                 }
-            }
-            else {
+
+                if (modifierFactory != null) {
+
+                    var modifier = modifierFactory.apply(value);
+
+                    if (inst.hasModifier(modifier)) {
+                        source.sendError(
+                                Text.translatable(
+                                        "commands.attribute.failed.modifier_already_present",
+                                        modifier.getId(),
+                                        Text.translatable(attribute.getTranslationKey()),
+                                        livingEntity.getName()
+                                )
+                        );
+                    }
+
+                    inst.addPersistentModifier(modifier);
+                } else {
+                    inst.setBaseValue(value);
+                }
+            } else {
                 source.sendError(Text.translatable("commands.attribute.failed.entity", target.getName()));
             }
         }, BINARY_RESULT_CONSUMER);
